2dec36356d9c22e05471b72dbc2fe3acbb3d4113
UnshiftedSet54 <smlrossales@gmail.com>
2023-05-06 17:06:46 +0100

    created html form

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var inputData = document.querySelectorAll('input');
+var nombre = inputData[0];
+var apellido = inputData[1];
+var id = inputData[2];
+var edad = inputData[3];
+var fechaNacimiento = inputData[4];
+var articulos = inputData[5];
---
+++ ts/main.ts
@@ -0,0 +1,9 @@
+const inputData: string[] = document.querySelectorAll('input')
+
+const nombre = inputData[0]
+const apellido = inputData[1]
+const id = inputData[2]
+const edad = inputData[3]
+const fechaNacimiento = inputData[4]
+const articulos = inputData[5]
+
