85b9a8e354613d101e4526a075e83d56e72bfdc5
UnshiftedSet54 <smlrossales@gmail.com>
2023-05-13 10:51:51 +0100

    new form data

--- js/main.js
+++ js/main.js
@@ -1,7 +1,57 @@
-var inputData = document.querySelectorAll('input');
-var nombre = inputData[0];
-var apellido = inputData[1];
-var id = inputData[2];
-var edad = inputData[3];
-var fechaNacimiento = inputData[4];
-var articulos = inputData[5];
+var SalesEmployee = /** @class */ (function () {
+    function SalesEmployee(nombre, apellido, correoElectronico, fechaNacimiento, unidadDeVenta, zonaGeografica) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correoElectronico = correoElectronico;
+        this.fechaNacimiento = fechaNacimiento;
+        this.unidadDeVenta = unidadDeVenta;
+        this.zonaGeografica = zonaGeografica;
+    }
+    SalesEmployee.prototype.getNombre = function () {
+        return this.nombre;
+    };
+    SalesEmployee.prototype.setNombre = function (newNombre) {
+        this.nombre = newNombre;
+    };
+    SalesEmployee.prototype.getApellido = function () {
+        return this.apellido;
+    };
+    SalesEmployee.prototype.setApellido = function (newApellido) {
+        this.apellido = newApellido;
+    };
+    SalesEmployee.prototype.getCorreoElectronico = function () {
+        return this.correoElectronico;
+    };
+    SalesEmployee.prototype.setCorreoElectronico = function (newCorreoElectronico) {
+        this.correoElectronico = newCorreoElectronico;
+    };
+    SalesEmployee.prototype.getFechaNacimiento = function () {
+        return this.fechaNacimiento;
+    };
+    SalesEmployee.prototype.setFechaNacimiento = function (newFechaNacimiento) {
+        this.fechaNacimiento = newFechaNacimiento;
+    };
+    SalesEmployee.prototype.getUnidadDeVenta = function () {
+        return this.unidadDeVenta;
+    };
+    SalesEmployee.prototype.setUnidadDeVenta = function (newUnidadDeVenta) {
+        this.unidadDeVenta = newUnidadDeVenta;
+    };
+    SalesEmployee.prototype.getZonaGeografica = function () {
+        return this.unidadDeVenta;
+    };
+    SalesEmployee.prototype.setZonaGeografica = function (newZonaGeografica) {
+        this.zonaGeografica = newZonaGeografica;
+    };
+    return SalesEmployee;
+}());
+var addEmployee = function () {
+    var nombre = document.getElementById('name').value;
+    var apellido = document.getElementById('surname').value;
+    var correoElectronico = document.getElementById('email').value;
+    var fechaNacimiento = new Date(document.getElementById('birthDate').value);
+    var unidadDeVenta = document.getElementById('sdUnit').value;
+    var zonaGeografica = document.getElementById('area').value;
+    var Empleado1 = new SalesEmployee(nombre, apellido, correoElectronico, fechaNacimiento, unidadDeVenta, zonaGeografica);
+    console.log(Empleado1);
+};
--- ts/main.ts
+++ ts/main.ts
@@ -1,9 +1,85 @@
-const inputData: string[] = document.querySelectorAll('input')
+interface Employee {
+  nombre: string
+  apellido: string
+  correoElectronico: string
+  fechaNacimiento: Date
+}
 
-const nombre = inputData[0]
-const apellido = inputData[1]
-const id = inputData[2]
-const edad = inputData[3]
-const fechaNacimiento = inputData[4]
-const articulos = inputData[5]
+class SalesEmployee implements Employee {
+  nombre: string
+  apellido: string
+  fechaNacimiento: Date
+  correoElectronico: string
+  unidadDeVenta: string
+  zonaGeografica: string
 
+  constructor(nombre: string, apellido: string, correoElectronico: string, fechaNacimiento: Date, unidadDeVenta: string, zonaGeografica: string){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.correoElectronico = correoElectronico
+    this.fechaNacimiento = fechaNacimiento
+    this.unidadDeVenta = unidadDeVenta
+    this.zonaGeografica = zonaGeografica
+  }
+
+  getNombre(){
+    return this.nombre
+  }
+
+  setNombre(newNombre: string){
+    this.nombre = newNombre
+  }
+
+  getApellido(){
+    return this.apellido
+  }
+
+  setApellido(newApellido: string){
+    this.apellido = newApellido
+  }
+
+  getCorreoElectronico(){
+    return this.correoElectronico
+  }
+
+  setCorreoElectronico(newCorreoElectronico: string){
+    this.correoElectronico = newCorreoElectronico
+  }
+
+  getFechaNacimiento(){
+    return this.fechaNacimiento
+  }
+
+  setFechaNacimiento(newFechaNacimiento: Date){
+    this.fechaNacimiento = newFechaNacimiento
+  }
+
+  getUnidadDeVenta(){
+    return this.unidadDeVenta
+  }
+
+  setUnidadDeVenta(newUnidadDeVenta){
+    this.unidadDeVenta = newUnidadDeVenta
+  }
+
+  getZonaGeografica(){
+    return this.unidadDeVenta
+  }
+
+  setZonaGeografica(newZonaGeografica){
+    this.zonaGeografica = newZonaGeografica
+  }
+}
+
+const addEmployee = (): void => {
+  const nombre: string = (<HTMLInputElement>document.getElementById('name')).value
+  const apellido: string = (<HTMLInputElement>document.getElementById('surname')).value
+  const correoElectronico: string = (<HTMLInputElement>document.getElementById('email')).value
+  const fechaNacimiento: Date = new Date((<HTMLInputElement>document.getElementById('birthDate')).value)
+  const unidadDeVenta: string = (<HTMLInputElement>document.getElementById('sdUnit')).value
+  const zonaGeografica: string = (<HTMLInputElement>document.getElementById('area')).value
+
+  const Empleado1: SalesEmployee = new SalesEmployee(nombre, apellido, correoElectronico, fechaNacimiento, unidadDeVenta, zonaGeografica)
+
+  console.log(Empleado1)
+}
